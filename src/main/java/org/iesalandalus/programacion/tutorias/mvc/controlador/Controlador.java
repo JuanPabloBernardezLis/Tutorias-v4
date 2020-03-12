d2768070cd810c93ae610735ab51c64d92dabb88
jpulu <jpulu@LAPTOP-OTS9BK3V>
2020-03-12 18:34:52 +0100

    controlador

--- src/main/java/org/iesalandalus/programacion/tutorias/mvc/controlador/Controlador.java
+++ src/main/java/org/iesalandalus/programacion/tutorias/mvc/controlador/Controlador.java
@@ -33,11 +33,13 @@ public class Controlador implements IControlador {
 	
 	@Override
 	public void comenzar() {
+		modelo.comenzar();
 		vista.comenzar();
 	}
 	
 	@Override
 	public void terminar() {
+		modelo.terminar();
 		System.out.println("Hasta luego Lucas!!!");
 	}
 	
